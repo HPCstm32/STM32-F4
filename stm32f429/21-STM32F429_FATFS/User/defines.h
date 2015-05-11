43a92ebe878f00e19d792745baadb6ba9d06af4b
Kao, Yu-Ting_MAC <jackzzjack@gmail.com>
2015-05-11 23:59:04 +0800

    Changelog: Add FATFS custom variables.

--- stm32f429/21-STM32F429_FATFS/User/defines.h
+++ stm32f429/21-STM32F429_FATFS/User/defines.h
@@ -30,4 +30,24 @@
 
 /* Put your global defines for all libraries here used in your project */
 
+/* Use SPI communication with SDCard */
+#define    FATFS_USE_SDIO                0
+
+/* Select your SPI settings */
+#define FATFS_SPI				SPI1
+#define FATFS_SPI_PINSPACK		TM_SPI_PinsPack_1
+/* The Default SPI setting is "SPI1 with Pins_pack 1". */
+
+/* Custom CS pin for SPI communication */	
+#define FATFS_CS_PORT		GPIOB
+#define FATFS_CS_PIN		GPIO_Pin_5
+/* SPI has a pin which is used to Select the Slave. We called it CS (Chip Select) or SS (Slave Select).
+	At STM32, we connect CS (SS) to GPIOB, Pin_5 (PB5) to control the High voltage or Low voltage. */
+
+// Use custom get_fattime() function
+#define FATFS_CUSTOM_FATTIME				1
+/* To enable the FAT Time with RTC */
+/* If you enable this option, you should implement the get_fattime() function */
+
+
 #endif
